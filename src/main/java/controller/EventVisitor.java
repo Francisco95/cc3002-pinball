d6e5ff60df51f8b813af92c4aa50d7bfe60d6c6b
Francisco95 <fjmunoz95@gmail.com>
2018-05-19 16:27:41 -0400

    modification in javadoc

--- src/main/java/controller/EventVisitor.java
+++ src/main/java/controller/EventVisitor.java
@@ -6,7 +6,9 @@ import main.java.logic.gameelements.Hittable;
 /**
  * Interface that define Visitor Pattern, in particular this define how the
  * visitor visit methods from different class, this is done combined with
- * observer pattern, idea comes from: <a href="https://stackoverflow.com/a/6608600">http://google.com</a>
+ * observer pattern, idea comes from: <a href="https://stackoverflow.com/a/6608600">http://google.com</a>.
+ * Game it will be the visitor and visit a Bonus or Hittable means that game will increase his
+ * score/balls by the corresponding amount.
  * @author Fancisco Muñoz Ponce. on date: 19-05-18
  */
 public interface EventVisitor {
