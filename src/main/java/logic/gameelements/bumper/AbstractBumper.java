2eb5a8f10f10c050dbe9457856032cc0b90ef7e2
Francisco95 <fjmunoz95@gmail.com>
2018-05-23 20:19:25 -0400

    abstracted some common behavior of bumpers

---
+++ src/main/java/logic/gameelements/bumper/AbstractBumper.java
@@ -0,0 +1,127 @@
+package logic.gameelements.bumper;
+
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Random;
+
+/**
+ * Abstraction of some common behavior of any bumper,
+ * since all the bumpers are used as observable,
+ * we use that class here.
+ * @author Fancisco Muñoz Ponce. on date: 23-05-18
+ */
+public abstract class AbstractBumper extends Observable implements Bumper{
+
+    /**
+     * keep count of the remaining hits the bumper has to receive to upgrade,
+     * when is 0 the bumper is upgraded
+     */
+    private int remainingHits;
+
+    /**
+     * the points given by the bumper, at first it's same
+     * points as basePoints, but when it is upgrades,
+     * score will be equal to UpgradedPoints;
+     */
+    private int score;
+
+    /**
+     * the random probability of trigger an {@link logic.bonus.ExtraBallBonus},
+     * this will be a random number between [0, 9] both included, since there is 10% of probability
+     * of trigger, the number should be 0.
+     */
+    private Random randomProb;
+
+    /**
+     * the seed for the random number, by default is declared as -1 which means that there
+     * is no seed set, if seed > -1 then set the seed
+     */
+    private int seed = -1;
+
+    public AbstractBumper(int remainingHits, int score){
+        this.score = score;
+        this.remainingHits = remainingHits;
+        randomProb = new Random();
+    }
+
+    /**
+     * as part of Observer Pattern, this set the new observers that observe this observable.
+     * @param observers instances of observers
+     */
+    public void setObservers(Observer...observers){
+        for (Observer o : observers)
+            addObserver(o);
+    }
+
+    /**
+     * check after a hit if the kicker bumper should be upgraded.
+     */
+    public void shouldUpgrade(){
+        if (remainingHits <= 0){
+            upgrade();
+        }
+    }
+
+    /**
+     * set the seed value of the random probability, this is usefull for
+     * testing purpose.
+     * @param seed the seed to use
+     */
+    public void setSeed(int seed){
+        this.seed = seed;
+    }
+
+    /**
+     * set the remaining hits needed to upgrade
+     * @param remainingHits the new remaining hits to upgrade
+     */
+    public void setRemainingHitsToUpgrade(int remainingHits){
+        this.remainingHits = remainingHits;
+    }
+
+    /**
+     * set the points given by the bumper
+     * @param newScore new score for the bumper
+     */
+    public void setScore(int newScore){
+        this.score = newScore;
+    }
+
+    public boolean compareScore(int score){
+        return this.score == score;
+    }
+
+    public boolean compareRemainingHits(int remainingHits){
+        return this.remainingHits == remainingHits;
+    }
+
+    @Override
+    public int remainingHitsToUpgrade() {
+        return remainingHits;
+    }
+
+    @Override
+    public void hit() {
+        this.remainingHits--;
+        shouldUpgrade();
+        setChanged();
+        notifyObservers(score);
+    }
+
+    @Override
+    public int getScore() {
+        return score;
+    }
+
+    /**
+     * for this case there is a probability of 1/10 of trigger an ExtraBallBonus when hit() is called.
+     * @return return True if the probability is satisfied.
+     */
+    @Override
+    public boolean bonusTriggered() {
+        if (seed > -1) {
+            randomProb.setSeed(seed);
+        }
+        return randomProb.nextInt(10) == 0;
+    }
+}
--- src/main/java/logic/gameelements/bumper/KickerBumper.java
+++ src/main/java/logic/gameelements/bumper/KickerBumper.java
@@ -14,113 +14,44 @@ import java.util.Random;
  * or to notify a trigger of bonus (ExtraBallBonus)
  * @author Fancisco Muñoz Ponce. on date: 17-05-18
  */
-public class KickerBumper extends Observable implements Bumper{
+public class KickerBumper extends AbstractBumper {
 
     /**
-     * keep count of the remaining hits the bumper has to receive to upgrade,
-     * when is 0 the bumper is upgraded
+     * the number of points given by the bumper not upgraded
      */
-    private int remainingHits;
+    private int baseScore;
 
     /**
-     * the number of points given by the kickerbumper
+     * the number of points given by the bumper when it's upgraded
      */
-    private int pointsGiven;
+    protected int upgradedScore;
 
     /**
-     * the random probability of trigger an {@link logic.bonus.ExtraBallBonus},
-     * this will be a random number between [0, 9] both included, since there is 10% of probability
-     * of trigger, the number should be 0.
+     * the number of hits needed to make an upgrade
      */
-    private Random randomProb;
+    protected int hitsNeededToUpgrade;
 
-    /**
-     * the seed for the random number, by default is declared as -1 which means that there
-     * is no seed set, if seed > -1 then set the seed
-     */
-    private int seed = -1;
-
-    public KickerBumper(){
-        this.remainingHits = 5;
-        this.pointsGiven = 500;
-        randomProb = new Random();
-    }
-
-    /**
-     * set the seed value of the random probability, this is usefull for
-     * testing purpose.
-     * @param seed the seed to use
-     */
-    public void setSeed(int seed){
-        this.seed = seed;
-    }
-
-    /**
-     * as part of Observer Pattern, this set the new observers that observe this observable.
-     * @param observers instances of observers
-     */
-    public void setObservers(Observer...observers){
-        for (Observer o : observers)
-            addObserver(o);
-    }
-
-    /**
-     * check after a hit if the kicker bumper should be upgraded.
-     */
-    public void shouldUpgrade(){
-        if (remainingHits <= 0){
-            upgrade();
-        }
-    }
-
-    public void setRemainingHitsToUpgrade(int remainingHits){
-        this.remainingHits = remainingHits;
-    }
-    @Override
-    public int remainingHitsToUpgrade() {
-        return remainingHits;
+    public KickerBumper() {
+        super(5, 500);
+        this.hitsNeededToUpgrade = 5;
+        this.baseScore = 500;
+        this.upgradedScore = 1000;
     }
 
     @Override
     public boolean isUpgraded() {
-        return remainingHits == 0 && pointsGiven == 1000;
+        return compareRemainingHits(0) && compareScore(upgradedScore);
     }
 
     @Override
     public void upgrade() {
-        remainingHits = 0;
-        this.pointsGiven = 1000;
+        setRemainingHitsToUpgrade(0);
+        setScore(upgradedScore);
     }
 
     @Override
     public void downgrade() {
-        this.pointsGiven = 500;
-        this.remainingHits = 5;
-    }
-
-    @Override
-    public void hit() {
-        this.remainingHits--;
-        shouldUpgrade();
-        setChanged();
-        notifyObservers(pointsGiven);
-
-    }
-
-    @Override
-    public int getScore() {
-        return pointsGiven;
-    }
-
-    /**
-     * for this case there is a probability of 1/10 of trigger an ExtraBallBonus when hit() is called.
-     * @return return True if the probability is satisfied.
-     */
-    @Override
-    public boolean bonusTriggered() {
-        if (seed > -1) {
-            randomProb.setSeed(seed);
-        }
-        return randomProb.nextInt(10) == 0;
+        setRemainingHitsToUpgrade(hitsNeededToUpgrade);
+        setScore(baseScore);
     }
 }
