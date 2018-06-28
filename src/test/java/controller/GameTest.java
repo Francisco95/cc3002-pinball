da91d36d230687f8520a4d3e6382a46ba178d4b0
Francisco95 <fjmunoz95@gmail.com>
2018-06-27 23:47:36 -0400

    created tests fron Game class, not completed

---
+++ src/test/java/controller/GameTest.java
@@ -0,0 +1,64 @@
+package controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Fancisco Muñoz Ponce. on date: 27-06-18
+ */
+public class GameTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void addToScore() {
+    }
+
+    @Test
+    public void getScore() {
+    }
+
+    @Test
+    public void setScore() {
+    }
+
+    @Test
+    public void addToBalls() {
+    }
+
+    @Test
+    public void getBalls() {
+    }
+
+    @Test
+    public void setBalls() {
+    }
+
+    @Test
+    public void visitHittable() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void visitBonus() {
+    }
+
+    @Test
+    public void visitBumper() {
+    }
+
+    @Test
+    public void visitTarget() {
+    }
+
+    @Test
+    public void visitTable() {
+    }
+}
