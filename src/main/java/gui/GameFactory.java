bd211b53116a3df663d742aedf428bceaf390069
Francisco Muñoz <fjmunoz95@gmail.com>
2018-08-12 16:00:56 -0400

    redefine the class GameFactory as a subclass of EntityFactory

--- src/main/java/gui/GameFactory.java
+++ src/main/java/gui/GameFactory.java
@@ -1,18 +1,17 @@
 package gui;
 
-import com.almasb.fxgl.entity.Entities;
-import com.almasb.fxgl.entity.Entity;
-import com.almasb.fxgl.entity.RenderLayer;
+import com.almasb.fxgl.entity.*;
 import com.almasb.fxgl.entity.components.CollidableComponent;
 import com.almasb.fxgl.physics.BoundingShape;
 import com.almasb.fxgl.physics.HitBox;
 import com.almasb.fxgl.physics.PhysicsComponent;
 import com.almasb.fxgl.physics.box2d.dynamics.BodyType;
-import com.almasb.fxgl.physics.box2d.dynamics.FixtureDef;
+import gui.components.BallControl;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.Shape;
+
+import static gui.Config.*;
 
 
 /**
@@ -23,49 +22,80 @@ import javafx.scene.shape.Shape;
  *
  * @author Francisco Munoz Ponce
  */
-public class GameFactory {
+public class GameFactory implements EntityFactory {
 
-    private static double ballRadius = 10;
+
+
 
     /**
      * Creation of a Ball, should be a circle that can collide with every elements
      * in the scene, this is the main object of the game.
      *
      * Should be declared as DYNAMIC type but due to problems with FXGL library it will
-     * be KINETIC type and the physical movement will be defined in {@link BallPhysics}
-     * where all the reaction to collisions will be defined
+     * be KINETIC type and the physical movement will be defined in {@link BallControl}
+     * and in the handler of collisions.
      *
-     * @param x X position at start
-     * @param y Y position at start
+     * @param data position of the entity
      * @return the BALL entity
      */
-    public static Entity ball(double x, double y){return null;}
+    @Spawns("Ball")
+    public Entity newBall(SpawnData data){
+        PhysicsComponent physics = new PhysicsComponent();
+        physics.setBodyType(BodyType.KINEMATIC);
+
+        return circleBuilder(BALL_RADIUS, COLOR_BALL, "Box")
+                .from(data)
+                .type(GameTypes.BALL)
+                .with(physics, new CollidableComponent(true))
+                .with(new BallControl())
+                .build();
+    }
 
     /**
+     * Creation of the four walls of the screen, these are limits on the movement of the ball
      *
-     * Creation of the four walls, these are just objects that can collide with others
-     *
-     * @param thickness define the hit box of the walls
      * @return the WALL entity
      */
-    public static Entity walls(int thickness){
-        Entity walls = Entities.makeScreenBounds(thickness);
+    @Spawns("TableLimits")
+    public Entity newTableLimits(SpawnData data){
+        Entity walls = Entities.makeScreenBounds(100);
         walls.setType(GameTypes.WALL);
         walls.addComponent(new CollidableComponent(true));
         return walls;
+    }
+
+    @Spawns("HorizontalWall")
+    public Entity newHorizontalWall(SpawnData data){
+        PhysicsComponent physics = new PhysicsComponent();
+        physics.setBodyType(BodyType.STATIC);
+        return boxBuilder(0, WIDTH, 20, Config.COLOR_BACKGROUND, "HorizontalWall")
+                .from(data)
+                .with(physics, new CollidableComponent(true))
+                .type(GameTypes.WALL)
+                .build();
+    }
+
+    @Spawns("VerticalWall")
+    public Entity newVerticalWall(SpawnData data){
+        PhysicsComponent physics = new PhysicsComponent();
+        physics.setBodyType(BodyType.STATIC);
+        return boxBuilder(0, 20, HEIGHT, Config.COLOR_BACKGROUND, "VerticalWall")
+                .from(data)
+                .with(physics, new CollidableComponent(true))
+                .type(GameTypes.WALL)
+                .build();
     }
 
     /**
      * Definition of the background of the platform (table), for
      * simplicity will be just a black background
      *
-     * @param width of the background
-     * @param height of the background
      * @return the background entity
      */
-    public static Entity background(int width, int height){
+    @Spawns("Background")
+    public Entity newBackground(SpawnData data){
         return Entities.builder()
-                .viewFromNode(new Rectangle(width, height, Color.BLACK))
+                .viewFromNode(new Rectangle(WIDTH, HEIGHT, COLOR_BACKGROUND))
                 .renderLayer(RenderLayer.BACKGROUND)
                 .build();
     }
@@ -75,11 +105,18 @@ public class GameFactory {
      * just rectangular figures which can rotate, color will
      * be a type of gray and the position will be fixed
      *
-     * @param x X position of the two flippers
-     * @param y Y position of the two flippers
      * @return the FLIPPER entity
      */
-    public static Entity flippers(double x, double y){return null;}
+    @Spawns("Flipper")
+    public Entity newFlipper(SpawnData data){
+        PhysicsComponent physics = new PhysicsComponent();
+        physics.setBodyType(BodyType.KINEMATIC);
+
+        return boxBuilder(0, FLIPPER_WIDTH, FLIPPER_HEIGHT, COLOR_FLIPPER, "Flipper")
+                .from(data)
+                .with(physics, new CollidableComponent(true))
+                .build();
+    }
 
     /**
      * Creation of the bumpers, with characteristics:
@@ -89,19 +126,19 @@ public class GameFactory {
      * pop    --------- circle --------
      * kicker --------- circle ------
      *
-     * @param x                                 X position of the Bumper
-     * @param y                                 Y position of the Bumper
-     * @param color                             color of the bumper (on ground level)
-     * @param shape                             Shape of the bumper
      * @return                                  the entity BUMPER
      */
-    public static Entity bumperShape(double x, double y, Color color, Shape shape){
-        PhysicsComponent physics = new PhysicsComponent();
-        physics.setBodyType(BodyType.STATIC);
-        physics.setFixtureDef(
-                new FixtureDef().density(0.4f).restitution(1.3f));
-        return boxBuilder(x, y, 0, ballRadius * 0.9, ballRadius * 0.9, "Bumper", color)
-                .with(physics, new CollidableComponent(true))
+    @Spawns("KickerBumper")
+    public Entity newKickerBumper(SpawnData data){
+        return newBumper(COLOR_KICKER_BUMPER_BASE)
+                .from(data)
+                .build();
+    }
+
+    @Spawns("PopBumper")
+    public Entity newPopBumper(SpawnData data){
+        return newBumper(COLOR_POP_BUMPER_BASE)
+                .from(data)
                 .build();
     }
 
@@ -113,68 +150,86 @@ public class GameFactory {
      * spot   -------- square (small) ---
      * drop   -------- square (small) -----
      *
-     * @param x                                 X position of the target
-     * @param y                                 Y position of the target
-     * @param color                             color of the target (on active mode)
-     * @param shape                             shape of the target
      * @return                                  the entity TARGET
      */
-    public static Entity targetShape(double x, double y, Color color, Shape shape){
-        PhysicsComponent physics = new PhysicsComponent();
-        physics.setBodyType(BodyType.STATIC);
-        physics.setFixtureDef(
-                new FixtureDef().density(0.4f).restitution(0.8f));
-        return boxBuilder(x, y, 0, ballRadius * 0.9, ballRadius * 0.9, "Target", color)
-                .with(physics, new CollidableComponent(true))
+    @Spawns("DropTarget")
+    public Entity newDropTarget(SpawnData data){
+        return newTarget(COLOR_DROP_TARGET_ACTIVE)
+                .from(data)
                 .build();
     }
 
+    @Spawns("SpotTarget")
+    public Entity newSpotTarget(SpawnData data){
+        return newTarget(COLOR_SPOT_TARGET_ACTIVE)
+                .from(data)
+                .build();
+    }
 
-    /**
-     * Additional creation of lines that could be useful, defined as statics
-     *
-     * @param x X position
-     * @param y Y position
-     * @param initAngle rotation of the object
-     * @param length length of the line
-     * @param color the color of the figure
-     * @return the entity line
-     */
-    public static Entity line(double x, double y, double initAngle, double length, Color color){
-        PhysicsComponent physics = new PhysicsComponent();
-        physics.setBodyType(BodyType.STATIC);
-        physics.setFixtureDef(
-                new FixtureDef().density(1f).restitution(0.8f));
-        return boxBuilder(x, y, initAngle, length, 1, "line", color)
-                .with(physics, new CollidableComponent(true))
+    @Spawns("SmallWall")
+    public Entity newSmallWall(SpawnData data){
+        return boxBuilder(0, WIDTH/4, 5, COLOR_WALL, "SmallWall")
+                .with(new CollidableComponent(true))
+                .from(data)
+                .type(GameTypes.WALL)
+                .build();
+    }
+
+    @Spawns("BigWall")
+    public Entity newBigWall(SpawnData data){
+        return boxBuilder(0, HEIGHT/4, WIDTH/4, COLOR_WALL, "BigWall")
+                .with(new CollidableComponent(true))
+                .from(data)
+                .type(GameTypes.WALL)
                 .build();
     }
 
     /**
      * Additional creation of box that could be useful, defined as statics
      *
-     * @param x X position
-     * @param y Y position
      * @param angle rotation of the object
      * @param width width of the rectangle
      * @param height length of the rectangle
      * @param color the color of the figure
-     * @return the entity rectangle
+     * @param name
+     * @return an EntityBuilder of a rectangle
      */
-    public static Entities.EntityBuilder boxBuilder(double x, double y, double angle, double width,
-                                                    double height, String name, Color color){
+    private static Entities.EntityBuilder boxBuilder(double angle, double width,
+                                                     double height, Color color, String name){
         return Entities.builder()
-                .at(x, y)
                 .rotate(angle)
                 .bbox(new HitBox(name, BoundingShape.box(width, height)))
                 .viewFromNode(new Rectangle(width, height, color));
     }
 
-    public static Entities.EntityBuilder circleBuilder(double x, double y, double rad, Color color, String name){
+    private static Entities.EntityBuilder squareBoxBuilder(double angle, double width, Color color, String name){
+        return boxBuilder(angle, width, width, color, name);
+    }
+
+    private static Entities.EntityBuilder circleBuilder(double rad, Color color, String name){
         return Entities.builder()
-                .at(x, y)
                 .bbox(new HitBox(name, BoundingShape.circle(rad)))
                 .viewFromNode(new Circle(rad, color));
+    }
+
+    private static Entities.EntityBuilder newBumper(Color bumperColor){
+        PhysicsComponent physics = new PhysicsComponent();
+        physics.setBodyType(BodyType.STATIC);
+//        physics.setFixtureDef(
+//                new FixtureDef().density(1f).restitution(1.3f));
+        return circleBuilder(BUMPER_RADIUS, bumperColor, "Bumper")
+                .with(physics, new CollidableComponent(true))
+                .type(GameTypes.BUMPER);
+    }
+
+    private static Entities.EntityBuilder newTarget(Color targetColor){
+        PhysicsComponent physics = new PhysicsComponent();
+        physics.setBodyType(BodyType.STATIC);
+//        physics.setFixtureDef(
+//                new FixtureDef().density(1f).restitution(1.3f));
+        return squareBoxBuilder(0, TARGET_WIDTH, targetColor, "Target")
+                .with(physics, new CollidableComponent(true))
+                .type(GameTypes.TARGET);
     }
 
 
