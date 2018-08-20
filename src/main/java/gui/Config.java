b7aa053b6aadfd6e97f075e935a9aa5bea80a112
Francisco Muñoz <fjmunoz95@gmail.com>
2018-08-19 23:23:42 -0300

    more variables in config file

--- src/main/java/gui/Config.java
+++ src/main/java/gui/Config.java
@@ -3,22 +3,25 @@ package gui;
 import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 
+/**
+ * Class that define those variables that can affect the entire game, these are "constants" used
+ * in different classes of the gui package.
+ *
+ * @author Francisco Munoz Ponce
+ */
 public class Config {
 
     private Config(){}
 
-
+    // basic stage variables (dimensions, background and gravity)
     public static final double WIDTH = 500;
-    public static final double HEIGHT = 800;
-
-    public static final double FLIPPER_WIDTH = 30;
-    public static final double FLIPPER_HEIGHT = 10;
-    public static final Color COLOR_FLIPPER = Color.IVORY;
-    public static final double FLIPPER_MAX_ROTATION_ANGLE = 50;
-    public static final double FLIPPER_LEFT_BASIC_ANGLE = -30;
-    public static final double FLIPPER_RIGHT_BASIC_ANGLE = 30;
+    public static final double HEIGHT = 900;
+    public static final Color COLOR_BACKGROUND = Color.BLACK;
+    public static final Point2D GRAVITY = new Point2D(0, 5);
+    public static final Color COLOR_WALL = Color.WHITESMOKE;
 
 
+    // game elements variables (dimensions and colors)
     public static final double BALL_RADIUS = 10;
     public static final Color COLOR_BALL = Color.SILVER;
 
@@ -33,13 +36,33 @@ public class Config {
     public static final Color COLOR_SPOT_TARGET_NON_ACTIVE = Color.DARKRED;
     public static final Color COLOR_DROP_TARGET_ACTIVE = Color.LIME;
     public static final Color COLOR_DROP_TARGET_NON_ACTIVE = Color.FORESTGREEN;
+    
+    
+    // flipper variables (dimensions, rotation limits and position)
+    public static final double FLIPPER_WIDTH = 160;
+    public static final double FLIPPER_HEIGHT = 10;
+    public static final Color COLOR_FLIPPER = Color.BLUEVIOLET;
+    public static final double FLIPPER_MAX_ROTATION_ANGLE = 70;
+    public static final double FLIPPER_RIGHT_BASIC_ANGLE = -30;
+    public static final double FLIPPER_LEFT_BASIC_ANGLE = 30;
+    public static final double FLIPPER_SPEED = 20;
+    public static final double FLIPPER_RIGHT_POSITION =  WIDTH - FLIPPER_WIDTH - 75;
+    public static final double FLIPPER_LEFT_POSITION = 75;
+    public static final double FLIPPER_VERTICAL_POSITION = HEIGHT - 110;
 
 
-    public static final Color COLOR_WALL = Color.WHITESMOKE;
-    public static final Color COLOR_BACKGROUND = Color.BLACK;
+    // timer times for automatic reset of game elements
+    public static final int N1 = 10;
+    public static final int N2 = 20;
+    public static final int N3 = 120;
 
-    public static final Point2D GRAVITY = new Point2D(5, 5);
 
+    /**
+     * class for define variables on the assets
+     */
+    public static final class Asset{
+        public static final String FXML_MAIN_UI = "main.fxml";
+    }
 
 
 }
