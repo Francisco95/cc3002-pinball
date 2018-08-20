a74eed611aea20697d1b2edae1242f55c718f5a4
Francisco Muñoz <fjmunoz95@gmail.com>
2018-08-19 23:24:37 -0300

    deleted not used basic events

--- src/main/java/gui/events/GameEvent.java
+++ src/main/java/gui/events/GameEvent.java
@@ -3,12 +3,14 @@ package gui.events;
 import javafx.event.Event;
 import javafx.event.EventType;
 
+/**
+ * Class that define some events of the game like Bonus events (JackPot, DropTarget and ExtraBall)
+ *
+ * @author Francisco Munoz Ponce
+ */
 public class GameEvent extends Event {
 
     public static final EventType<GameEvent> ANY = new EventType<>(Event.ANY, "GAME_EVENT");
-    public static final EventType<GameEvent> DROP_BALL = new EventType<>(ANY, "DROP_BALL");
-    public static final EventType<GameEvent> NEW_BALL = new EventType<>(ANY, "NEW_BALL");
-    public static final EventType<GameEvent> END_GAME = new EventType<>(ANY, "END_GAME");
     public static final EventType<GameEvent> DROP_TARGET_BONUS = new EventType<>(ANY, "DROP_TARGET_BONUS");
     public static final EventType<GameEvent> EXTRA_BALL_BONUS = new EventType<>(ANY, "EXTRA_BALL_BONUS");
     public static final EventType<GameEvent> JACK_POT_BONUS = new EventType<>(ANY, "JACK_POT_BONUS");
